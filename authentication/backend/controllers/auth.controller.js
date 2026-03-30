96945f848016cd762fd61c45ca7c97496edcda6b
abhissshk <abhishek629460@gmail.com>
2026-03-30 14:24:01 +0530

    fixed login cookie and error handling

--- authentication/backend/controllers/auth.controller.js
+++ authentication/backend/controllers/auth.controller.js
@@ -62,53 +62,44 @@ message:"user created"
 
 
 // login
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-const login=async(req,res)=>{
-  try{
-
-    const{email,password}=req.body
-    let existUser=await User.findOne({email})
-    if(!existUser){
-      return res.status(400).json({message:"user does not exist"})
+    let existUser = await User.findOne({ email });
+    if (!existUser) {
+      return res.status(400).json({ message: "user does not exist" });
     }
 
-    let match=await bcrypt.compare(password,existUser.password)
-    if(!match){
-      return res.status(400).json({message:"Incorrect password"})
+    let match = await bcrypt.compare(password, existUser.password);
+    if (!match) {
+      return res.status(400).json({ message: "Incorrect password" });
     }
 
-    
-  let token
-try{
-      token=genrateToken(existUser._id)
-}catch(err){
-  console.log(err)
-}
+    let token = genrateToken(existUser._id);
 
-res.cookie("token",token,{
-      httpOnly:true,
-      secure:process.env.NODE_ENVIRONMENT=="production",
-      sameSite:"strict",
-      maxAge:7*24*60*60*1000
-    })
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
 
-    
-return res.status(201).json({user:{firstName:existUser.firstName, 
-  lastName:existUser.lastName,
-   email:existUser.email,
-   userName:existUser.userName,
-profileImage:existUser.profileImage
-},
-message:"login succusefully"
-})
+    return res.status(200).json({
+      user: {
+        firstName: existUser.firstName,
+        lastName: existUser.lastName,
+        email: existUser.email,
+        userName: existUser.userName,
+        profileImage: existUser.profileImage
+      },
+      message: "login successfully"
+    });
 
-
-  }catch(err){
-return res.status(500).json(err)
+  } catch (err) {
+    return res.status(500).json({ message: "internal server error" });
   }
-
-}
-
+};
 const logout=async(req,res)=>{
   try{
 res.clearCookie("token")
